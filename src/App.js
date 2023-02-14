30fb71b26cebf1ed03ef3f3b8ee602b995bab8a3
HaiAz <abcxyz123789654@gmail.com>
2023-02-14 21:01:17 +0700

    ?

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
     return (
         <div className="App">
             <AppProvider>
-                <Login />
+                <Home />
             </AppProvider>
         </div>
     );
