0d4e98ec905cad259c6b38a16f307328b4ac9baf
HaiAz <abcxyz123789654@gmail.com>
2023-05-06 18:35:56 +0700

    quizz page

--- src/pages/Quizz/Quizz.jsx
+++ src/pages/Quizz/Quizz.jsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { AiOutlineFlag } from "react-icons/ai";
 import { HiOutlineArrowSmLeft, HiOutlineArrowSmRight } from "react-icons/hi";
 import { useParams } from "react-router-dom";
-import { db } from "../../firebase/config";
+import { db, auth } from "../../firebase/config";
+import { useDispatch, useSelector } from "react-redux";
+import { useAppContext } from "../../context/AppProvider";
+import { getDoc, doc } from "firebase/firestore";
 export default function Quizz() {
     const { id } = useParams();
-    // const examRef = collection(db, `exams`)
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.authSlice.user);
+    const [currentQuestion, setCurrentQuestion] = useState(1);
+    const [listQuestion, setListQuestion] = useState();
+    const [filterQuestion, setFilterQuestion] = useState(listQuestion);
+
+    const { setTitile } = useAppContext();
+
+    useEffect(() => {
+        const historyRef = doc(db, "histories", `${auth.currentUser.uid}/exams/${id}`);
+        const history = getDoc(historyRef);
+
+        const questionRef = doc(db, "questions", `${history.subject}/questions/`);
+    }, []);
     return (
-        <div>
-            <div className="ml-8 mt-4">
+        <div className="w-full h-screen overflow-hidden ">
+            <div className="flex flex-col justify-center items-start ml-8 mt-4">
                 <h1 className="text-3xl">Câu 1</h1>
                 <div className="flex mt-6">
                     <input type="radio" name="radio-5" className="radio radio-success" />
@@ -31,7 +47,8 @@ export default function Quizz() {
                 <span className="mr-8">Phân vân</span>
                 <AiOutlineFlag className="text-3xl" />
             </div>
-            <div className="flex mx-32 mt-12">
+
+            <div className="flex justify-center items-center">
                 <div className="flex justify-center items-center mx-4 my-2 px-4 py-2">
                     <button className="btn btn-info btn-outline text-2xl mx-4 my-2">
                         <span className="pr-3">Back</span>
